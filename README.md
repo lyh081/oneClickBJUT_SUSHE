e2b8ac80f5c7373320711a3b3f8c8435094d26b7
jianjun_mac <lyhdut@163.com>
2022-04-27 15:36:12 +0800

    V1.0

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# oneClickBJUT_SUSHE
-一键登录北工大中蓝校区无线网
+# oneClickBJUT：一键登录中蓝校区无线网
+
+## 背景
+
+最近因为疫情被封在中蓝宿舍。中蓝的无线网每次都要登录两遍，而且每次电脑一息屏就要重新登录，非常烦人。于是就写了一个Chrome插件，保存信息之后，就可以一键登录了。
+
+## 安装
+
+1. 下载源码
+
+   仓库地址：[oneClickBJUT_SUSHE](https://github.com/lyh081/oneClickBJUT_SUSHE）
+
+   或者：
+
+   ```shell
+   git clone git@github.com:lyh081/oneClickBJUT_SUSHE.git
+   ```
+
+2. 安装拓展
+
+   由于Chrome禁止安装未上传Chrome商店的crx格式插件，所以需要在开发者模式中加载已经解压的扩展程序。
+
+   1. 在扩展程序界面（地址栏输入chrome://extensions/）点击右上角开发者模型按钮打开 **开发者模式；**
+   
+   2. 选择左上角 **加载已解压的扩展程序** ，在弹出框中选择下载的仓库文件夹中的extension文件夹。
+   
+      💡 **一定选择extension文件夹！**
+   
+   3. 关闭开发者模式
+
+## 使用
+
+在扩展页面中填入学号、BJUT_SUSHE_5/2.4G密码、校园网网关密码（lgn.bjut.edu.cn），**点击保存** ，再选择一键登录。
+
+💡 **两个密码一样的话，校园网网关密码可不填**
+
+## 说明
+
+1. 学号、密码等信息均保存在chrome本地，本拓展不存储任何个人信息。
+2. 吐槽一下BJUT_SUSHE_5/2.4G登录的时候直接把学号密码明文放在GET的查询里面……
+
---
+++ extension/js/popup.js
@@ -0,0 +1,115 @@
+
+let post = document.getElementById("post")
+let save = document.getElementById("save")
+let reset = document.getElementById("reset")
+
+checkPlaceHolder();
+
+post.addEventListener("click", () => {
+    post.style.backgroundColor = "#cecece";
+    lgn2sushe();
+});
+
+save.addEventListener("click", () => {
+    save.style.backgroundColor = "#3264B7";
+    save.setAttribute("disabled", "disabled");
+    var sid = document.getElementById("sid").value;
+    var sushe = document.getElementById("sushe").value;
+    var bjut = document.getElementById("bjut").value;
+    if (bjut.length === 0) {
+        bjut = sushe;
+    };
+    chrome.storage.local.set({ sid: sid, sushe: sushe, bjut: bjut }, () => {
+        checkPlaceHolder();
+        alert("保存成功!");
+    });
+
+});
+
+reset.addEventListener("click", () => {
+    chrome.storage.local.remove(["sid", "sushe", "bjut"], () => {
+        checkPlaceHolder();
+        alert("重置成功，请重新保存学号密码!");
+        inputs = ["sid", "sushe", "bjut"]
+        for (let i of inputs) {
+            let input = document.getElementById(i);
+            input.removeAttribute("disabled");
+            if (i !== "sid") {
+                input.setAttribute("type", "text")
+            }
+        }
+    });
+});
+
+function checkPlaceHolder() {
+    chrome.storage.local.get(["sid", "sushe", "bjut"], (data) => {
+        // 未保存学号密码
+        if (data["sid"] === undefined || data["sushe"] === undefined || data["bjut"] === undefined) {
+            reset.style.backgroundColor = "#cecece";
+            reset.setAttribute("disabled", "disabled")
+            save.style.backgroundColor = "#4285f4";
+            save.removeAttribute("disabled");
+            // 已经保存学号密码
+        } else {
+            inputs = ["sid", "sushe", "bjut"]
+            for (let i of inputs) {
+                let input = document.getElementById(i);
+                input.setAttribute("value", data[i]);
+                input.setAttribute("disabled", "disabled");
+                if (i !== "sid") {
+                    input.setAttribute("type", "password")
+                }
+            }
+            save.style.backgroundColor = "#cecece";
+            save.setAttribute("disabled", "disabled");
+            reset.style.backgroundColor = "#4285f4";
+            reset.removeAttribute("disabled");
+        }
+    });
+};
+
+
+
+function lgn2sushe() {
+    chrome.storage.local.get(["sid", "sushe"], (data) => {
+        let sid = data["sid"];
+        let sushe = data["sushe"];
+        let url = "http://10.21.221.98:801/eportal/?c=Portal&a=login&callback=dr1651022956866&login_method=1&user_account=" + sid + "@campus&user_password=" + sushe + "&jsVersion=3.0&_=1651022938835";
+        let options = {
+            method: "GET",
+            header: {
+                Accept: "*/*"
+            }
+        }
+        fetch(url, options)
+            .then((res) => {
+                if (res.status === 200) {
+                    lgn2bjut();
+                } else {
+                    alert("登录BJUT_SUSHE失败,请检查学号密码是否准确")
+                }
+            })
+    })
+}
+
+function lgn2bjut() {
+
+    chrome.storage.local.get(["sid", "bjut"], (data) => {
+        let sid = data["sid"];
+        let bjut = data["bjut"];
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://lgn.bjut.edu.cn/', true);
+        xhr.send("DDDDD="+ sid + "&upass="+ bjut +"&0MKKey=");
+
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                alert("登录成功！")
+            }else if(xhr.status !== 200){
+                alert("登录lgn.bjut.edu.cn失败,请检查账号密码");
+            }
+
+        }       
+    })
+
+}
+
